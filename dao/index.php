18e7052f5eef1f5d0d49ad0d58b4b1a691a291ea
Yagamii <yagogomesc@gmail.com>
2018-01-29 16:39:05 -0200

    Feito aula de select do dao

--- dao/index.php
+++ dao/index.php
@@ -2,10 +2,27 @@
 
     require_once("config.php");
 
-    $root = new Usuario();
+  //  $root = new Usuario();
 
-    $root->loadById(3);
+  //  $root->loadById(3);
 
-    echo $root;
+  //  echo $root;
+
+  //Carrega lista de Usuario
+  //$lista = Usuario::getList();
+
+  //echo json_encode($lista);
+
+  //Carrega lista de usuario por $login
+
+  //$search = Usuario::search("t");
+
+  //echo json_encode($search);
+
+  //Carrega um usuário
+  $usuario = new Usuario();
+  $usuario->login("teste","123456");
+
+  echo $usuario;
 
  ?>
